c865bd99b2459bf05c13b4d3612c31493c9a4ebd
youngyin <57608725+youngyin@users.noreply.github.com>
2023-12-03 07:07:16 +0900

    feat/swagger - auth #11
    - show auth-controller on swagger-page

--- src/main/kotlin/com/meokq/api/core/config/SwaggerConfig.kt
+++ src/main/kotlin/com/meokq/api/core/config/SwaggerConfig.kt
@@ -1,19 +1,27 @@
 package com.meokq.api.core.config
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@SecurityScheme(
+    type = SecuritySchemeType.APIKEY, `in` = SecuritySchemeIn.HEADER,
+    name = "authorization", description = "Auth Token",
+)
 class SwaggerConfig {
 
     @Bean
     fun publicApi(): GroupedOpenApi =
         GroupedOpenApi.builder()
             .group("paste-public")
-            .pathsToMatch("/api/**")
+            .pathsToMatch("/api/**", "/auth/**")
             .build()
 
     @Bean
@@ -23,6 +31,12 @@ class SwaggerConfig {
                 Info()
                     .title("Meok-q Api Document")
                     //.description("")
-                    .version("v1")
+                    .version("v2")
+            )
+            .security(
+                listOf(
+                    SecurityRequirement()
+                        .addList("authorization")
+                )
             )
 }
