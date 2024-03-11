3e81ca71fab4c565a18d895dd20e80763e51a133
username <email@email.com>
2024-03-11 15:44:51 +0900

    [fix] 더미데이터 logo_image_id 존재하는 값으로 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO tb_market (market_id, `name`, address, district, content, phone, ticket_count, logo_image_id, status, president_id)
 VALUES
-    ('MK00000001', 'Market Name', '서울특별시 종로구', '1100000000', 'Market Content', '0211111111', 10, 'IM10000003', 'APPROVED', 'BS10000001'),
-    ('MK00000002', 'Market Name', '서울특별시 종로구', '1100000000', 'Market Content', '0222222222', 10, 'IM10000003', 'APPROVED', 'BS10000001'),
-    ('MK00000003', 'Market Name', '서울특별시 종로구', '1111000000', 'Market Content', '0233333333', 10, 'IM10000003', 'APPROVED', 'BS10000001'),
-    ('MK00000004', 'Market Name', '서울특별시 종로구', '1100000000', 'Market Content', '0244444444', 10, 'IM10000003', 'UNDER_REVIEW', 'BS10000001'),
-    ('MK00000005', 'Market Name', '서울특별시 종로구', '1111000000', 'Market Content', '0255555555', 10, 'IM10000003', 'UNDER_REVIEW', 'BS10000002'),
-    ('MK00000010', 'Market Name', '서울특별시 종로구', '1111000000', 'Market Content', '0255555555', 10, 'IM10000003', 'REGISTERED', 'BS10000002');
+    ('MK00000001', 'Market Name', '서울특별시 종로구', '1100000000', 'Market Content', '0211111111', 10, 'IMMA2024031114564633', 'APPROVED', 'BS10000001'),
+    ('MK00000002', 'Market Name', '서울특별시 종로구', '1100000000', 'Market Content', '0222222222', 10, 'IMMA2024031114564633', 'APPROVED', 'BS10000001'),
+    ('MK00000003', 'Market Name', '서울특별시 종로구', '1111000000', 'Market Content', '0233333333', 10, 'IMMA2024031114564633', 'APPROVED', 'BS10000001'),
+    ('MK00000004', 'Market Name', '서울특별시 종로구', '1100000000', 'Market Content', '0244444444', 10, 'IMMA2024031114564633', 'UNDER_REVIEW', 'BS10000001'),
+    ('MK00000005', 'Market Name', '서울특별시 종로구', '1111000000', 'Market Content', '0255555555', 10, 'IMMA2024031114564633', 'UNDER_REVIEW', 'BS10000002'),
+    ('MK00000010', 'Market Name', '서울특별시 종로구', '1111000000', 'Market Content', '0255555555', 10, 'IMMA2024031114564633', 'REGISTERED', 'BS10000002');
 ;
 
 INSERT INTO tb_quest (quest_id, status, market_id, create_date, update_date)
@@ -32,10 +32,10 @@ VALUES
 -- Insert data into tb_image table
 INSERT INTO tb_image (file_id, `type`, `size`)
 VALUES
-    ('IM10000001', 'BUSINESS_REGISTRATION_CERTIFICATE', 1024),
-    ('IM10000002', 'ID_CARD', 2048),
-    ('IM10000003', 'MARKET_LOGO', 3072),
-    ('IM10000004', 'RECEIPT', 3072);
+    ('IMMA2024031114590814', 'BUSINESS_REGISTRATION_CERTIFICATE', 1024),
+    ('IMMA2024031114593704', 'ID_CARD', 2048),
+    ('IMMA2024031114564633', 'MARKET_LOGO', 3072),
+    ('IMMA2024031115000583', 'RECEIPT', 3072);
 
 -- Insert data into tb_customer table
 INSERT INTO tb_customer (customer_id, status, email, nickname)
@@ -58,11 +58,11 @@ VALUES
 -- Insert data into tb_challenge_history table
 INSERT INTO tb_challenge_history (challenge_id, customer_id, status, reject_reason, quest_id, receipt_image_id)
 VALUES
-    ('CH10000001', 'CS10000001', 'UNDER_REVIEW', NULL, 'QS10000001', 'IM10000001'),
-    ('CH10000002', 'CS10000001', 'UNDER_REVIEW', NULL, 'QS10000002', 'IM10000002'),
-    ('CH10000003', 'CS10000002', 'APPROVED', NULL, 'QS10000002', 'IM10000003'),
-    ('CH10000004', 'CS10000003', 'APPROVED', NULL, 'QS10000002', 'IM10000003'),
-    ('CH10000005', 'CS10000001', 'APPROVED', NULL, 'QS10000002', 'IM10000002');
+    ('CH10000001', 'CS10000001', 'UNDER_REVIEW', NULL, 'QS10000001', 'IMMA2024031114590814'),
+    ('CH10000002', 'CS10000001', 'UNDER_REVIEW', NULL, 'QS10000002', 'IMMA2024031114593704'),
+    ('CH10000003', 'CS10000002', 'APPROVED', NULL, 'QS10000002', 'IMMA2024031114564633'),
+    ('CH10000004', 'CS10000003', 'APPROVED', NULL, 'QS10000002', 'IMMA2024031114564633'),
+    ('CH10000005', 'CS10000001', 'APPROVED', NULL, 'QS10000002', 'IMMA2024031114593704');
 
 -- Insert data into tb_boss table
 INSERT INTO tb_boss (boss_id, status, email)
@@ -77,42 +77,42 @@ VALUES
     ('CP10000003', 'CH10000005', 'ISSUED', 'RW00000002', 'MK00000001', 'CS10000001', 'QS10000002');
 
 INSERT INTO tb_market_time (week_day, market_id, open_time, close_time, holiday_yn) VALUES
-('MON', 'MK00000001', '09:00', '20:00', 'N'),
-('TUE', 'MK00000001', '09:00', '18:00', 'N'),
-('WED', 'MK00000001', '09:00', '18:00', 'N'),
-('THU', 'MK00000001', '09:00', '18:00', 'N'),
-('FRI', 'MK00000001', '09:00', '18:00', 'N'),
-('SAT', 'MK00000001', '09:00', '18:00', 'N'),
-('SUN', 'MK00000001', '09:00', '20:00', 'Y'),
-('MON', 'MK00000002', '10:00', '20:00', 'N'),
-('TUE', 'MK00000002', '10:00', '18:00', 'N'),
-('WED', 'MK00000002', '09:00', '18:00', 'N'),
-('THU', 'MK00000002', '09:00', '18:00', 'N'),
-('FRI', 'MK00000002', '09:00', '18:00', 'N'),
-('SAT', 'MK00000002', '09:00', '18:00', 'N'),
-('SUN', 'MK00000002', '10:00', '20:00', 'Y'),
-('MON', 'MK00000003', '10:00', '20:00', 'N'),
-('TUE', 'MK00000003', '10:00', '18:00', 'N'),
-('WED', 'MK00000003', '09:00', '18:00', 'N'),
-('THU', 'MK00000003', '09:00', '18:00', 'N'),
-('FRI', 'MK00000003', '09:00', '18:00', 'N'),
-('SAT', 'MK00000003', '09:00', '18:00', 'N'),
-('SUN', 'MK00000003', '10:00', '20:00', 'Y')
+    ('MON', 'MK00000001', '09:00', '20:00', 'N'),
+    ('TUE', 'MK00000001', '09:00', '18:00', 'N'),
+    ('WED', 'MK00000001', '09:00', '18:00', 'N'),
+    ('THU', 'MK00000001', '09:00', '18:00', 'N'),
+    ('FRI', 'MK00000001', '09:00', '18:00', 'N'),
+    ('SAT', 'MK00000001', '09:00', '18:00', 'N'),
+    ('SUN', 'MK00000001', '09:00', '20:00', 'Y'),
+    ('MON', 'MK00000002', '10:00', '20:00', 'N'),
+    ('TUE', 'MK00000002', '10:00', '18:00', 'N'),
+    ('WED', 'MK00000002', '09:00', '18:00', 'N'),
+    ('THU', 'MK00000002', '09:00', '18:00', 'N'),
+    ('FRI', 'MK00000002', '09:00', '18:00', 'N'),
+    ('SAT', 'MK00000002', '09:00', '18:00', 'N'),
+    ('SUN', 'MK00000002', '10:00', '20:00', 'Y'),
+    ('MON', 'MK00000003', '10:00', '20:00', 'N'),
+    ('TUE', 'MK00000003', '10:00', '18:00', 'N'),
+    ('WED', 'MK00000003', '09:00', '18:00', 'N'),
+    ('THU', 'MK00000003', '09:00', '18:00', 'N'),
+    ('FRI', 'MK00000003', '09:00', '18:00', 'N'),
+    ('SAT', 'MK00000003', '09:00', '18:00', 'N'),
+    ('SUN', 'MK00000003', '10:00', '20:00', 'Y')
 ;
 
 -- Insert data into tb_operator_info
 INSERT INTO tb_operator_info (record_id, owner_name, owner_birthdate, idcard_image_id)
 VALUES
-    ('PR00000001', 'John Doe', '19900101', 'IM10000002'),
-    ('PR00000002', 'Jane Smith', '19850515', 'IM10000002'),
-    ('PR00000003', 'Bob Johnson', '19780930', 'IM10000002');
+    ('PR00000001', 'John Doe', '19900101', 'IMMA2024031114593704'),
+    ('PR00000002', 'Jane Smith', '19850515', 'IMMA2024031114593704'),
+    ('PR00000003', 'Bob Johnson', '19780930', 'IMMA2024031114593704');
 
 -- Insert data into tb_business_report
 INSERT INTO tb_license_business (record_id, license_id, license_image_id, owner_name, market_name, address, postal_code)
 VALUES
-    ('BR00000001', 'LICENSE123', 'IM10000001', 'John Doe', 'Doe Market', '123 Main St', '12345'),
-    ('BR00000002', 'LICENSE456', 'IM10000001', 'Jane Smith', 'Smith Market', '456 Oak St', '67890'),
-    ('BR00000003', 'LICENSE789', 'IM10000001', 'Bob Johnson', 'Johnson Market', '789 Elm St', '34567');
+    ('BR00000001', 'LICENSE123', 'IMMA2024031114590814', 'John Doe', 'Doe Market', '123 Main St', '12345'),
+    ('BR00000002', 'LICENSE456', 'IMMA2024031114590814', 'Jane Smith', 'Smith Market', '456 Oak St', '67890'),
+    ('BR00000003', 'LICENSE789', 'IMMA2024031114590814', 'Bob Johnson', 'Johnson Market', '789 Elm St', '34567');
 
 -- Insert data into tb_market_auth_history
 INSERT INTO tb_market_auth_history (record_id, market_id, review_result, comment, operator_info_id, license_info_id)
@@ -156,3 +156,4 @@ VALUES
     ('15', 'BS10000002', 'TERMS_OF_SERVICE_BOSS', 1, 'Y',now()),
     ('16', 'BS10000002', 'UNIQUEID_CONSENT_FORM_BOSS', 1, 'N',now()),
     ('17', 'BS10000002', 'PRIVACY_POLICY_BOSS', 1, 'Y',now());
+
