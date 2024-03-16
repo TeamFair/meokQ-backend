ece591bed1401825010e5e9ded6ae8a7b67d5936
youngyin <57608725+youngyin@users.noreply.github.com>
2024-03-17 00:08:01 +0900

    [feat] IQU001-quest 만기일시 추가

--- src/main/kotlin/com/meokq/api/quest/response/QuestListResp.kt
+++ src/main/kotlin/com/meokq/api/quest/response/QuestListResp.kt
@@ -1,5 +1,6 @@
 package com.meokq.api.quest.response
 
+import com.meokq.api.core.converter.DateTimeConverterV2
 import com.meokq.api.quest.enums.MissionType
 import com.meokq.api.quest.enums.QuestStatus
 import com.meokq.api.quest.enums.RewardType
@@ -11,6 +12,7 @@ class QuestListResp(
     var missionTitle : String?,
     var rewardTitle : String?,
     var status: QuestStatus?,
+    var expireDate: String?,
 ) {
 
     constructor(model: Quest) : this(
@@ -19,5 +21,6 @@ class QuestListResp(
         missionTitle = model.missions?.firstOrNull()?.let { MissionType.getTitle(it) },
         rewardTitle = model.rewards?.firstOrNull()?.let { RewardType.getTitle(it) },
         status = model.status,
+        expireDate = model.expireDate?.let { DateTimeConverterV2.convertToString(it) }
     )
 }
