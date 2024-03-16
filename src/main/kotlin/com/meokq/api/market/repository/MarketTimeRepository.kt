907031b79629abf9d28133b0ccc646c3361f022f
youngyin <57608725+youngyin@users.noreply.github.com>
2024-03-16 23:07:00 +0900

    [fix] delete 쿼리 개선 #32

--- src/main/kotlin/com/meokq/api/market/repository/MarketTimeRepository.kt
+++ src/main/kotlin/com/meokq/api/market/repository/MarketTimeRepository.kt
@@ -3,8 +3,13 @@ package com.meokq.api.market.repository
 import com.meokq.api.market.model.MarketTime
 import com.meokq.api.market.model.identifier.MarketTimeId
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface MarketTimeRepository : JpaRepository<MarketTime, MarketTimeId> {
     fun findAllByMarketId(marketId : String) : List<MarketTime>
+
+    @Modifying
+    @Query("delete from tb_market_time where marketId=:marketId")
     fun deleteAllByMarketId(marketId: String) : Int
 }
