3da40673e49656f2a7e3f54e57f55b8c0e24359c
youngyin <57608725+youngyin@users.noreply.github.com>
2024-06-08 19:54:43 +0900

    [feat] 경험치 타입의 보상 추가 및 저장 로직 테스트 #59

--- src/main/kotlin/com/meokq/api/quest/enums/RewardType.kt
+++ src/main/kotlin/com/meokq/api/quest/enums/RewardType.kt
@@ -4,7 +4,8 @@ import com.meokq.api.quest.model.Reward
 
 enum class RewardType {
     GIFT, // 증정
-    DISCOUNT // 할인
+    DISCOUNT, // 할인
+    XP, // 경험치 부여
     ;
 
     companion object {
@@ -12,6 +13,7 @@ enum class RewardType {
             return when(reward.type){
                 GIFT -> return "${reward.target} ${reward.quantity}개(잔) 증정권"
                 DISCOUNT -> return "${reward.target} ${reward.discountRate}% 할인권"
+                XP -> return "${reward.quantity}xp 경험치 부여"
                 else -> {throw Exception("지원하지 않는 type 입니다.") }
             }
         }
--- src/test/kotlin/com/meokq/api/quest/service/QuestServiceTest.kt
+++ src/test/kotlin/com/meokq/api/quest/service/QuestServiceTest.kt
@@ -3,13 +3,21 @@ package com.meokq.api.quest.service
 import com.meokq.api.TestData.missionReqForSave1
 import com.meokq.api.TestData.missionReqForSave2
 import com.meokq.api.TestData.rewardReqForSave1
+import com.meokq.api.auth.enums.UserType
+import com.meokq.api.auth.request.AuthReq
+import com.meokq.api.market.model.Market
+import com.meokq.api.market.service.MarketService
 import com.meokq.api.quest.enums.MissionType
 import com.meokq.api.quest.enums.RewardType
 import com.meokq.api.quest.model.Mission
 import com.meokq.api.quest.model.Reward
 import com.meokq.api.quest.request.QuestCreateReq
+import com.meokq.api.quest.request.QuestCreateReqForAdmin
 import com.meokq.api.quest.request.QuestSearchDto
+import com.meokq.api.quest.request.RewardReq
+import com.meokq.api.user.service.BossService
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -22,8 +30,9 @@ import org.springframework.transaction.annotation.Transactional
 @ActiveProfiles("local")
 @Transactional
 internal class QuestServiceTest {
-    @Autowired
-    private lateinit var service: QuestService
+    @Autowired private lateinit var service: QuestService
+    @Autowired private lateinit var bossService: BossService
+    @Autowired private lateinit var marketService: MarketService
 
     @Test
     //@Transactional
@@ -75,6 +84,62 @@ internal class QuestServiceTest {
         val rewardTitles = req.rewards.map { RewardType.getTitle(Reward(it)) }
         Assertions.assertIterableEquals(rewardTitles, searchData.rewardTitles)
     }
+
+    @Test
+    @DisplayName("경험치를 제공하는 퀘스트를 등록합니다.")
+    fun registerExperienceTypeQuest(){
+        // given
+        val market = marketService.saveModel(Market())
+        val xpReward = RewardReq(
+            type = RewardType.XP,
+            target = null,
+            quantity = 30,
+            content = null,
+            discountRate = null,
+        )
+        val questReq = QuestCreateReq(
+            marketId = market.marketId!!,
+            missions = listOf(missionReqForSave1),
+            rewards = listOf(xpReward),
+        )
+
+        // when
+        val questResp1 = service.save(questReq)
+        val findQuest1 = service.findById(questResp1.questId!!)
+
+        Assertions.assertEquals(questReq.marketId, findQuest1.marketId)
+        Assertions.assertTrue{findQuest1.missionTitles?.isNotEmpty() == true}
+        Assertions.assertTrue{findQuest1.rewardTitles?.isNotEmpty() == true}
+    }
+
+    @Test
+    @DisplayName("경험치를 제공하는 퀘스트를 등록합니다.(관리자 등록시)")
+    fun registerExperienceTypeQuestAdmin(){
+        // given
+        val xpReward = RewardReq(
+            type = RewardType.XP,
+            target = null,
+            quantity = 30,
+            content = null,
+            discountRate = null,
+        )
+        val adminReq = QuestCreateReqForAdmin(
+            missions = listOf(missionReqForSave1),
+            rewards = listOf(xpReward),
+            expireDate = "9999-12-31"
+        )
+        val adminAuthReq = AuthReq(
+            userType = UserType.ADMIN
+        )
+
+        // when
+        val questResp2 = service.adminSave(adminReq, adminAuthReq)
+        val findQuest2 = service.findById(questResp2.questId!!)
+
+        Assertions.assertTrue{findQuest2.missionTitles?.isNotEmpty() == true}
+        Assertions.assertTrue{findQuest2.rewardTitles?.isNotEmpty() == true}
+    }
+
 
     @Test
     //@Transactional
