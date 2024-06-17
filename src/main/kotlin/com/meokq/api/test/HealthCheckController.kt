dddc3047cead9feafa83234e350d98474393c6c7
youngyin <57608725+youngyin@users.noreply.github.com>
2024-06-17 22:59:53 +0900

    [feat] healthcheck controller 임의생성

---
+++ src/main/kotlin/com/meokq/api/test/HealthCheckController.kt
@@ -0,0 +1,16 @@
+package com.meokq.api.test
+
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Tag(name = "healthCheck", description = "테스트용 컨트롤러")
+@Controller
+class HealthCheckController{
+
+    @GetMapping("/api/open/healthCheck")
+    fun open(): ResponseEntity<String> {
+        return ResponseEntity.ok().body("OK")
+    }
+}
