9ce9fff0f287a93c3816fa736ea609057507d030
antidote <ljh8651@nate.com>
2024-07-08 15:54:37 +0900

    [fix] 이모지 테스트 코드 수정

--- src/test/kotlin/com/meokq/api/emoji/service/EmojiServiceTest.kt
+++ src/test/kotlin/com/meokq/api/emoji/service/EmojiServiceTest.kt
@@ -2,18 +2,25 @@ package com.meokq.api.emoji.service
 
 import com.meokq.api.auth.enums.UserType
 import com.meokq.api.auth.request.AuthReq
+import com.meokq.api.core.exception.NotUniqueException
 import com.meokq.api.emoji.request.EmojiRegisterReq
+import com.meokq.api.emoji.response.EmojiDefaultResp
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.Rollback
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.transaction.annotation.Transactional
 
-@Transactional
+
 @SpringBootTest
+@Transactional
 @ActiveProfiles("local")
+@Rollback
 class EmojiServiceTest{
 
     @Autowired
@@ -22,37 +29,108 @@ class EmojiServiceTest{
         userType = UserType.CUSTOMER,
         userId = "110804aa-a3f9-4894-93d9-9b446e583b27",
     )
+    val customerAuthReq2 =  AuthReq(
+        userType = UserType.CUSTOMER,
+        userId = "c89e0d8a-fb8d-4fd8-bd76-867d3fbce427",
+    )
+    val customerAuthReq3 =  AuthReq(
+        userType = UserType.CUSTOMER,
+        userId = "d3f0d5dc-22b2-472e-b1f7-fd6212b62cb7",
+    )
+    val customerAuthReq4 =  AuthReq(
+        userType = UserType.CUSTOMER,
+        userId = "ce19b5b9-a975-402b-bbf3-5e87cc41ef07",
+    )
+
 
     val targetId = "CH10000001"
+    lateinit var testSavedEmoji: EmojiDefaultResp
 
-    @Test
-    @DisplayName("LIKE 이모지를 등록 합니다.")
-    fun saveLike() {
+    @BeforeEach
+    fun setUp() {
+        // save 메서드 실행
         val req = EmojiRegisterReq(
             targetId = "CH10000001",
             targetType = "challenge",
             emojiType = "like"
         )
-        val resp = emojiService.register(customerAuthReq,req)
+        val req2 = EmojiRegisterReq(
+            targetId = "CH10000001",
+            targetType = "challenge",
+            emojiType = "like"
+        )
+        val req3 = EmojiRegisterReq(
+            targetId = "CH10000001",
+            targetType = "challenge",
+            emojiType = "like"
+        )
+        val req4 = EmojiRegisterReq(
+            targetId = "CH10000001",
+            targetType = "challenge",
+            emojiType = "like"
+        )
+        val req5 = EmojiRegisterReq(
+            targetId = "CH10000001",
+            targetType = "challenge",
+            emojiType = "hate"
+        )
+        val req6 = EmojiRegisterReq(
+            targetId = "CH10000001",
+            targetType = "challenge",
+            emojiType = "hate"
+        )
+        testSavedEmoji = emojiService.register(customerAuthReq,req)
+        emojiService.register(customerAuthReq2,req2)
+        emojiService.register(customerAuthReq3,req3)
+        emojiService.register(customerAuthReq4,req4)
+        emojiService.register(customerAuthReq,req5)
+        emojiService.register(customerAuthReq2,req6)
+
+    }
+
+    @Test
+    @DisplayName("LIKE 이모지를 등록 합니다.")
+    fun saveLike() {
+        val saveReq = EmojiRegisterReq(
+            targetId = "CH10000002",
+            targetType = "challenge",
+            emojiType = "like"
+        )
+        val resp = emojiService.register(customerAuthReq,saveReq)
 
         assertNotNull(resp)
-        assertEquals(req.targetType.uppercase(),resp.targetType)
-        assertEquals(req.emojiType.uppercase(),resp.emojiStatus)
+        assertEquals(saveReq.targetType.uppercase(),resp.targetType.toString())
+        assertEquals(saveReq.emojiType.uppercase(),resp.emojiStatus.toString())
+    }
+
+    @Test
+    @DisplayName("이모지는 중복될 수 없습니다.")
+    fun saveLike2() {
+        val req = EmojiRegisterReq(
+            targetId = "CH10000001",
+            targetType = "challenge",
+            emojiType = "like"
+        )
+        // 같은 요청으로 두 번째 호출 시 예외 발생 예상
+        val exception = assertThrows<NotUniqueException> {
+            emojiService.register(customerAuthReq, req)
+        }
+        assertEquals("이미 등록된 이모지 입니다.", exception.message)
     }
 
     @Test
     @DisplayName("HATE 이모지를 등록 합니다.")
     fun saveHate() {
         val req = EmojiRegisterReq(
-            targetId = "CH10000001",
+            targetId = "CH10000002",
             targetType = "challenge",
             emojiType = "hate"
         )
         val resp = emojiService.register(customerAuthReq,req)
 
         assertNotNull(resp)
-        assertEquals(req.targetType.uppercase(),resp.targetType)
-        assertEquals(req.emojiType.uppercase(),resp.emojiStatus)
+        assertEquals(req.targetType.uppercase(),resp.targetType.toString())
+        assertEquals(req.emojiType.uppercase(),resp.emojiStatus.toString())
     }
 
     @Test
@@ -62,40 +140,13 @@ class EmojiServiceTest{
 
         assertNotNull(resp)
         assertTrue(resp.isLike)
-        assertTrue(resp.isHate)
-    }
-
-    @Test
-    @DisplayName("유저가 해당 Target에 이모지를 등록 하지 않았는지 나와야 합니다.")
-    fun getEmojiFalse() {
-        val customerAuthReq =  AuthReq(
-            userType = UserType.CUSTOMER,
-            userId = "f6744202-f40f-4ce7-b00f-1a8d10456454",
-        )
-        val resp = emojiService.getEmoji(customerAuthReq,targetId)
-
-        assertNotNull(resp)
-        assertTrue(resp.isLike)
-        assertFalse(resp.isHate)
     }
 
     @Test
     @DisplayName("유저가 Target의 이모지를 삭제 합니다.")
     fun deleteEmoji() {
-        val emojiId = "f47ac10b-58cc-4372-a567-0e02b2c3d479"
 
-        emojiService.delete(customerAuthReq,emojiId)
-
-        val resp = emojiService.getEmoji(customerAuthReq,targetId)
-        assertFalse(resp.isLike)
-    }
-
-    @Test
-    @DisplayName("유저가 해당 이모지의 주인이 아니면 삭제할 수 없습니다.")
-    fun deleteFalseEmoji() {
-        val emojiId = "f47ac10b-58cc-4372-a567-0e02b2c3d479"
-
-        emojiService.delete(customerAuthReq,emojiId)
+        emojiService.delete(customerAuthReq,testSavedEmoji.emojiId!!)
 
         val resp = emojiService.getEmoji(customerAuthReq,targetId)
         assertFalse(resp.isLike)
@@ -103,6 +154,7 @@ class EmojiServiceTest{
 
     @Test
     @DisplayName("Target이 삭제되면 해당 이모지 모두가 삭제 됩니다.")
+
     fun deleteAllByTargetId(){
         emojiService.deleteAllByTargetId(targetId)
         val resp = emojiService.getModels(targetId)
