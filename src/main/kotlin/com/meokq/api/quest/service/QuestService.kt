fba6d52ecf276de2104ac6f20f2c81e72a95f88d
antidote <ljh8651@nate.com>
2024-07-15 21:55:44 +0900

    [fix] 완료한 퀘스트가 없을 시 미완료한 퀘스트가 안뜨는 버그 해결

--- src/main/kotlin/com/meokq/api/quest/service/QuestService.kt
+++ src/main/kotlin/com/meokq/api/quest/service/QuestService.kt
@@ -1,13 +1,9 @@
 package com.meokq.api.quest.service
 
 import com.meokq.api.auth.request.AuthReq
-import com.meokq.api.core.DataValidation.checkNotNullData
 import com.meokq.api.core.JpaService
 import com.meokq.api.core.JpaSpecificationService
 import com.meokq.api.core.repository.BaseRepository
-import com.meokq.api.file.enums.ImageType
-import com.meokq.api.file.request.ImageReq
-import com.meokq.api.file.service.ImageService
 import com.meokq.api.quest.enums.QuestStatus
 import com.meokq.api.quest.model.Quest
 import com.meokq.api.quest.repository.QuestHistoryRepository
@@ -20,12 +16,11 @@ import com.meokq.api.quest.response.QuestDeleteResp
 import com.meokq.api.quest.response.QuestDetailResp
 import com.meokq.api.quest.response.QuestListResp
 import com.meokq.api.quest.specification.QuestSpecification
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.multipart.MultipartFile
 
 @Service
 class QuestService(
@@ -38,6 +33,9 @@ class QuestService(
     override var jpaRepository: JpaRepository<Quest, String> = repository
     override val jpaSpecRepository: BaseRepository<Quest, String> = repository
     private val specifications = QuestSpecification
+    private companion object{
+        val EMPTYUSERLIST = listOf("empty")
+    }
 
     fun findAll(searchDto: QuestSearchDto, pageable: Pageable): PageImpl<QuestListResp> {
         val specification = specifications.bySearchDto(searchDto)
@@ -105,13 +103,13 @@ class QuestService(
         return PageImpl(responses, pageable, questHistories.totalElements)
     }
 
-    fun getUncompletedQuests(pageable: Pageable, authReq: AuthReq): PageImpl<QuestListResp> {
-        val questHistories = questHistoryRepository.findByCustomerId(authReq.userId!!,pageable)
+    fun getUncompletedQuests(pageable: Pageable, authReq: AuthReq): Page<QuestListResp> {
+        val questHistories = questHistoryRepository.findByCustomerId(authReq.userId!!, pageable)
         val questIds = questHistories.content.map { it.questId!! }
-        val models = repository.findAllByQuestIdNotInAndStatus(questIds,QuestStatus.PUBLISHED)
+        val models = repository.findAllByQuestIdNotInAndStatus(questIds.ifEmpty { EMPTYUSERLIST }, QuestStatus.PUBLISHED, pageable)
         val responses = models.map { QuestListResp(it) }
 
-        return PageImpl(responses, pageable, questHistories.totalElements)
+        return responses
     }
 
     fun delete(questId: String): QuestDeleteResp {
