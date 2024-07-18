c17d6c2ef54a03c261cecd4ac6524a7e8cc7f8fb
antidote <ljh8651@nate.com>
2024-07-18 21:34:33 +0900

    [fix] 챌린지 알고리즘 메서드명 수정

--- src/main/kotlin/com/meokq/api/rank/EmojiRankService.kt
+++ src/main/kotlin/com/meokq/api/rank/EmojiRankService.kt
@@ -1,9 +1,12 @@
 package com.meokq.api.rank
 
+import com.meokq.api.challenge.model.Challenge
+
 interface EmojiRankService<T> {
 
     var upperRank: MutableSet<T>
 
     var lowerRank: MutableSet<T>
     fun addToRank(item: T)
+    fun fetchShuffleRankToPage(): Set<Challenge>
 }
