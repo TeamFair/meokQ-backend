aab9b5b5404253d5fb055f521e1cb16add52c2a0
tj3828 <tj3828@naver.com>
2024-11-28 22:01:09 +0900

    [fix] 랜덤 조회시 기준 변경 & DB 데이터 조회로 변경

--- src/main/kotlin/com/meokq/api/challenge/repository/ChallengeRepository.kt
+++ src/main/kotlin/com/meokq/api/challenge/repository/ChallengeRepository.kt
@@ -5,11 +5,17 @@ import com.meokq.api.challenge.model.Challenge
 import com.meokq.api.core.repository.BaseRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
 
 interface ChallengeRepository : BaseRepository<Challenge, String> {
     fun findAllByStatus(status: ChallengeStatus): List<Challenge>
     fun deleteAllByQuestId(questId:String)
     fun findAllByQuestId(questId: String): List<Challenge>
     fun countByCustomerIdAndStatus(customerId: String, status: ChallengeStatus): Long
-
+    @Query("""
+        SELECT c
+        FROM Challenge AS c
+        ORDER BY CASE WHEN c.likeEmojiCnt = 0 THEN 1 ELSE 0 END ASC, c.updateDate DESC
+    """)
+    fun findAllRandomChallenge(pageable: Pageable): Page<Challenge>
 }
